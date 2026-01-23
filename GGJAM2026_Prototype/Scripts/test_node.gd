d5cd7c2a29c77f5a1938fff6dfae1bbbc562cda0
Joaquin Meló <joaquinmelo90@gmail.com>
2026-01-23 17:20:30 -0300

    Setup prototype
    Se creo la primera escena con placeholders para testeas funcionalidad del mouse hoover.
    Funciono como se esperaba.
    
    Proximo paso: crear eventos para el click del mouse.

---
+++ GGJAM2026_Prototype/Scripts/test_node.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var hand_cursor = preload("res://Assets/Images/HandCursor.png")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func change_cursor_hand():
+	Input.set_custom_mouse_cursor(hand_cursor)
+	
+func change_cursor_back():
+	Input.set_custom_mouse_cursor(null)
